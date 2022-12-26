7196b227fe1109cc58980166a6df9e252a6bd4b3
Marc Gonzalez Vidal <69361348+Gonsa02@users.noreply.github.com>
2022-12-26 19:31:28 +0100

    Add files via upload

---
+++ Practica3/generador_fichero_problema.cc
@@ -0,0 +1,187 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+typedef vector<vector<bool>> Matrix;
+
+void i_es_connex(const Matrix &M, vector<bool> &v, int n) {
+    if (not v[n]) { v[n] = true;
+	for (int i = 0; i < M.size(); ++i) if (M[n][i]) i_es_connex(M, v, i);
+    }
+}
+
+bool es_connex(const Matrix &M) {
+    vector<bool> v = vector<bool>(M.size(), false);
+    i_es_connex(M, v, 0);
+    for (int i = 0; i < v.size(); ++i) if (not v[i]) return false;
+    return true;
+}
+
+int main() {
+    
+    int num_rovers, num_asentamientos, num_almacen, num_suministros, num_personal, num_pet_sum, num_pet_pers, semilla;
+    cout << "Introduce una semilla: ";
+    cin >> semilla;
+    srand(semilla);
+    cout << "Introduce el numero de rovers: ";
+    cin >> num_rovers;
+    cout << endl << "Introduce el numero de asentamientos: ";
+    cin >> num_asentamientos;
+    cout << endl << "Introduce el numero de almacenes: ";
+    cin >> num_almacen;
+    cout << endl << "Introduce el numero de suministros: ";
+    cin >> num_suministros;
+    cout << endl << "Introduce el numero de personal: ";
+    cin >> num_personal;
+    cout << endl << "Introduce el numero de peticiones de suministro: ";
+    cin >> num_pet_sum;
+    cout << endl << "Introduce el numero de peticiones del personal: ";
+    cin >> num_pet_pers;
+    cout << endl;
+ 
+    //Vamos a generar el grafo del mapa
+    Matrix M(num_almacen+num_asentamientos, vector<bool>(num_almacen+num_asentamientos, false));
+    
+    //Primero vamos a asegurarnos de que el grafo es conexo
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    if (i == j-1 and j-1 >= 0) M[i][j] = true;
+	    if (i == j+1 and j+1 < M.size()) M[i][j] = true;
+	}
+    }
+
+    //Escribo grafo
+    /*
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    cout << M[i][j] << " ";
+	}
+	cout << endl;
+    }
+    cout << endl;
+    */
+
+    //Añadimos aristas random con una probabilidad del 35%
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    if (i != j and rand()%100 <= 35) {
+		M[i][j] = true;
+		M[j][i] = true;
+	    }
+	}
+    }
+
+    //Quitamos aristas random con una probabilidad del 10% de forma que el grafo sigue siendo connexo
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    if (i != j) {
+		if (rand()%100 <= 10) {
+		    M[i][j] = false;
+		    M[j][i] = false;
+		}
+		if (not es_connex(M)) {
+		    M[i][j] = true;
+		    M[j][i] = true;
+		}
+	    }
+	}
+    }
+
+    //Escribo grafo
+    /*
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    cout << M[i][j] << " ";
+	}
+	cout << endl;
+    }
+    cout << endl;
+    */
+
+    cout << "(define (problem ext1)" << endl;
+    cout << "(:domain ext1)" << endl;
+
+    // OBJECTS
+    cout << "(:objects" << endl;
+    cout << "		";
+    for (int i = 0; i < num_rovers; ++i) cout << "r" << i+1 << " ";
+    cout << "- Rover" << endl;
+    cout << "		";
+    for (int i = 0; i < num_asentamientos; ++i) cout << "as" << i+1 << " ";
+    cout << "- Asentamiento" << endl;
+    cout << "		";
+    for (int i = 0; i < num_almacen; ++i) cout << "al" << i+1 << " ";
+    cout << "- Almacen" << endl;
+    cout << "		";
+    for (int i = 0; i < num_suministros; ++i) cout << "s" << i+1 << " ";
+    cout << "- Suministro" << endl;
+    cout << "		";
+    for (int i = 0; i < num_personal; ++i) cout << "pers" << i+1 << " ";
+    cout << "- Personal" << endl;
+    cout << "		";
+    for (int i = 0; i < num_pet_sum; ++i) cout << "ps" << i+1 << " ";
+    cout << "- pSuministro" << endl;
+    cout << "		";
+    for (int i = 0; i < num_pet_pers; ++i) cout << "pp" << i+1 << " ";
+    cout << "- pPersonal" << endl;
+    cout << "		";
+    cout << endl;
+    cout << ")" << endl;
+    cout << endl;
+
+    // INIT
+    cout << "(:init" << endl;
+    for (int i = 0; i < num_rovers; ++i) cout << "		" << "(= (capacidad r" << i+1 << ") 2)" << endl;
+    cout << endl;
+    for (int i = 0; i < num_rovers; ++i) cout << "		" << "(= (combustible r" << i+1 << ") " << (rand()%num_almacen+num_asentamientos)+1 << ")" << endl;
+    cout << endl;
+    cout << "		" << "(= (prioridad-total) 0)" << endl;
+    cout << endl;
+
+    for (int i = 0; i < num_rovers; ++i) {
+	cout << "		" << "(aparcado r" << i+1 << " ";
+	int aux = rand()%M.size();
+	if (aux < num_asentamientos) cout << "as" << aux+1;
+	else cout << "al" << aux-num_asentamientos+1;
+	cout << ")" << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < num_almacen+num_asentamientos; ++i) {
+	for (int j = 0; j < num_almacen+num_asentamientos; ++j) {
+	    if (M[i][j]) {
+		cout << "		" << "(accesible ";
+		if (i < num_asentamientos) cout << "as" << i+1;
+		else cout << "al" << i-num_asentamientos+1;
+		cout << " ";
+		if (j < num_asentamientos) cout << "as" << j+1;
+		else cout << "al" << j-num_asentamientos+1;
+		cout << ")" << endl;
+	    }
+	}
+    }
+    cout << endl;
+
+    for (int i = 0; i < num_personal; ++i) cout << "		" << "(esta pers" << i+1 << " as" << (rand()%num_asentamientos)+1 << ")" << endl;
+    for (int i = 0; i < num_suministros; ++i) cout <<  "		" << "(esta s" << i+1 << " al" << (rand()%num_almacen)+1 << ")" << endl;
+    cout << endl;
+
+    for (int i = 0; i < num_personal; ++i) cout << "		" << "(libre p" << i+1 << ")" << endl;
+    cout << endl;
+
+    for (int i = 0; i < num_pet_sum+num_pet_pers; ++i) cout << "		" << "(objetivo p" << i+1 << " as" << (rand()%num_asentamientos)+1 << ")" << endl;
+    cout << endl;
+
+    cout << ")" << endl;
+    cout << endl;
+
+    // GOAL
+    cout << "(:goal TRUE)" << endl;
+    cout << endl;
+
+    cout << "(:metric maximize (prioridad-total))" << endl;
+    cout << endl;
+    cout << ")" << endl;
+}
